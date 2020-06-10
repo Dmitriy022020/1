8a4bb81dbec755d8b4d5f69398ca6e37189b53b2
Dmitrii Bagaev <dmitrii.bagaev@accenture.com>
2020-06-10 15:43:53 +0300

    19 localStorage ok

--- src/components/myFilms/MyFilmList.jsx
+++ src/components/myFilms/MyFilmList.jsx
@@ -1,121 +1,122 @@
-import React, { Component } from 'react';
+import React, {useEffect, useState} from 'react';
 import './books.css';
 import '../film/films.css';
-import {connect} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import MyFilm from "./MyFilm";
+import {addMyFilm} from "../../stores/actions";
 
-class MyFilmList extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      name: '',
-      author: '',
-      genre: '',
-      isOpen: false,
-      sort: '',
-    }
+function MyFilmList() {
+  const myFilms = useSelector(state => state.allFilms.myFilms);
+  const dispatch = useDispatch()
+  const [name, setName] = useState('');
+  const [author, setAuthor] = useState('');
+  const [genre, setGenre] = useState('');
+  const [sort, setSort] = useState('');
+  const [state, setState] = useState(false);
+
+  if (myFilms.length === 0) {
+    const saved = JSON.parse(localStorage.getItem('myFilms') || '[]')
+    dispatch(addMyFilm(saved))
+  }
+  useEffect(() => {
+    localStorage.setItem('myFilms', JSON.stringify(myFilms))
+  }, [myFilms])
+
+  const nameSearchOn = event => {
+    setName(event.target.value)
   };
-  nameSearchOn = event => {
-    this.setState({ name: event.target.value })
+  const authorSelect = event => {
+    setAuthor(event.target.value)
   };
-  authorSelect = event => {
-    this.setState({ author: event.target.value })
+  const genreSelect = event => {
+    setGenre(event.target.value)
   };
-  genreSelect = event => {
-    this.setState({ genre: event.target.value })
+  const sortSelect = event => {
+    setSort(event.target.value)
   };
-  sortSelect = event => {
-    this.setState({ sort: event.target.value })
+  const handleClick = () => {
+    setState(!state)
   };
-  handleClick = () => {
-    this.setState({
-      isOpen: !this.state.isOpen
-    })
+ // const {isOpen} = state;
+  const nameSearch = (item) => {
+    const valueName = name.toLowerCase();
+    if (valueName === '') return true;
+    else if (item.name.toLowerCase().includes(valueName)) return true
   };
-  render() {
-    const { isOpen } = this.state;
-    const nameSearch = (item) => {
-      const valueName = this.state.name.toLowerCase();
-      if (valueName === '') return true;
-      else if (item.name.toLowerCase().includes(valueName)) return true
-    };
-    const authorFilter = (item) => {
-      const valueAuthor = this.state.author;
-      return valueAuthor === '' || valueAuthor === item.author
-    };
-    const genreFilter = (item) => {
-      const valueGenre = this.state.genre;
-      return valueGenre === '' || item.genre === valueGenre
-    };
-    const listSort = (a, b) => {
-      const value = this.state.sort;
-      if (a[value] > b[value]) return 1;
-      if (a[value] < b[value]) return -1;
-      return 0
-    };
-    const filmsFilter = this.props.myFilms.filter(nameSearch).filter(authorFilter).filter(genreFilter).sort(listSort)
-    const elem = filmsFilter.map(film =>
-      <li key={film.id} className="film">
-        <MyFilm film={film}/>
-      </li>
-    );
-    const authorElem = [...new Set(this.props.myFilms.map(item => item.author))].map(
-      (author) => <option key={author}>{author}</option>
-    );
-    const genreElem = [...new Set(this.props.myFilms.map(item => item.genre))].map(
-      (genre) => <option key={genre}>{genre}</option>
-    );
-    console.log(this.props.myFilms)
-    const tableFilter = isOpen &&
-      <form className='filter'>
-        <div>
-          <label>Поиск</label>
-          <input value={this.state.name} type='text' placeholder="введите наименование" onChange={this.nameSearchOn} />
-        </div>
-        <div>
-          <label>Автор</label>
-          <select value={this.state.author} onChange={this.authorSelect}>
-            <option value=''>все</option>
-            {authorElem}
-          </select>
-        </div>
-        <div>
-          <label>Жанр</label>
-          <select value={this.state.genre} onChange={this.genreSelect}>
-            <option value=''>все</option>
-            {genreElem}
-          </select>
-        </div>
-        <div>
-          <label>Сортировать</label>
-          <select value={this.state.sort} onChange={this.sortSelect}>
-            <option value='id'>по умолчанию (id)</option>
-            <option value='name'>по названию</option>
-            <option value='author'>по автору</option>
-          </select>
-        </div>
-      </form>
-    return (
-      <div className="container">
-        <h2>Книги</h2>
-        <legend>Поиск и фильтр
-          <button className="button" onClick={this.handleClick}>
-            {isOpen ? 'закрыть' : 'открыть'}
-          </button>
-        </legend>
-        {tableFilter}
-        <ul className="ul_f">
-          {elem}
-        </ul>
+  const authorFilter = (item) => {
+    const valueAuthor = author;
+    return valueAuthor === '' || valueAuthor === item.author
+  };
+  const genreFilter = (item) => {
+    const valueGenre = genre;
+    return valueGenre === '' || item.genre === valueGenre
+  };
+  const listSort = (a, b) => {
+    const value = sort;
+    if (a[value] > b[value]) return 1;
+    if (a[value] < b[value]) return -1;
+    return 0
+  };
+  const filmsFilter = myFilms.filter(nameSearch).filter(authorFilter).filter(genreFilter).sort(listSort)
+  const elem = filmsFilter.map(film =>
+    <li key={film.id} className="film">
+      <MyFilm film={film}/>
+    </li>
+  );
+  const authorElem = [...new Set(myFilms.map(item => item.author))].map(
+    (author) => <option key={author}>{author}</option>
+  );
+  const genreElem = [...new Set(myFilms.map(item => item.genre))].map(
+    (genre) => <option key={genre}>{genre}</option>
+  );
+  console.log(myFilms)
+  const tableFilter = state &&
+    <form className='filter'>
+      <div>
+        <label>Поиск</label>
+        <input
+          value={name}
+          type='text'
+          placeholder="введите наименование"
+          onChange={nameSearchOn}/>
       </div>
-    );
-  };
+      <div>
+        <label>Автор</label>
+        <select value={author} onChange={authorSelect}>
+          <option value=''>все</option>
+          {authorElem}
+        </select>
+      </div>
+      <div>
+        <label>Жанр</label>
+        <select value={genre} onChange={genreSelect}>
+          <option value=''>все</option>
+          {genreElem}
+        </select>
+      </div>
+      <div>
+        <label>Сортировать</label>
+        <select value={sort} onChange={sortSelect}>
+          <option value='id'>по умолчанию (id)</option>
+          <option value='name'>по названию</option>
+          <option value='author'>по автору</option>
+        </select>
+      </div>
+    </form>
+  return (
+    <div className="container">
+      <h2>Книги</h2>
+      <legend>Поиск и фильтр
+        <button className="button" onClick={handleClick}>
+          {state ? 'закрыть' : 'открыть'}
+        </button>
+      </legend>
+      {tableFilter}
+      <ul className="ul_f">
+        {elem}
+      </ul>
+    </div>
+  );
 };
 
-const mapStateToProps = state => ({
-  myFilms: state.allFilms.myFilms
-})
-const mapDispatchToProps = {
-
-}
-export default connect(mapStateToProps, mapDispatchToProps)(MyFilmList)
+export default MyFilmList
